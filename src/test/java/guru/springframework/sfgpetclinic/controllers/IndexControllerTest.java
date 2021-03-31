4a98548e1edbf0b1ac675702338cff008bdf1237
Developer <jjstrain68@gmail.com>
2021-03-31 18:59:41 +0100

    Added Index Controller tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+    @BeforeEach
+    void setUp(){
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+
+        assertEquals("index",indexController.index(),"Wrong view returned");
+    }
+
+    @Test
+    void oupsHandler() {
+        //assertEquals("notimplemented",indexController.oupsHandler(),"Wrong view returned");
+        assertTrue("notimplemented".equalsIgnoreCase(indexController.oupsHandler()),() -> "Wrong view returned");
+    }
+}
