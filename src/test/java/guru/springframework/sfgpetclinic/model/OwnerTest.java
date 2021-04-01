bb369e7194a1e9e11dec47617f57e3b4365ce167
Developer <jjstrain68@gmail.com>
2021-04-01 15:50:58 +0100

    Nested Tests using OwnerMapServiceTest

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,12 +1,14 @@
 package guru.springframework.sfgpetclinic.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@Tag("model")
 class OwnerTest {
 
     private Owner owner;
