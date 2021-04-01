1993ffab099b3e46dec39f0471b9121ab1e07a89
Developer <jjstrain68@gmail.com>
2021-04-01 10:59:22 +0100

    Added tests using test assumptions, typically dependent on environment data/setup

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
 
@@ -55,5 +56,15 @@ class IndexControllerTest {
 
             System.out.println("I got here 2342342342342");
         });
+    }
+
+    @Test
+    void testAssumptionTrue(){
+        assumeTrue("GURU".equalsIgnoreCase(System.getenv("GURU_RUNTIME")));
+    }
+
+    @Test
+    void testAssumptionIsTrue(){
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
     }
 }
